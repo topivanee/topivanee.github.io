2d76aff3f437cba758515d73b06453f195b92b53
topivanee <ivanegacherdi@gmail.com>
2026-07-25 12:00:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -27,39 +27,25 @@ behavior:"smooth"
 // Create particles
 
 
-const particles =
-document.getElementById("particles");
+const particles = document.getElementById("particles");
 
-
+const fragment = document.createDocumentFragment();
 
 for(let i = 0; i < 80; i++){
 
+    let particle = document.createElement("div");
 
-let particle =
-document.createElement("div");
+    particle.className = "particle";
 
+    particle.style.left = Math.random() * 100 + "%";
 
+    particle.style.animationDuration =
+        (5 + Math.random() * 10) + "s";
 
-particle.className="particle";
+    particle.style.animationDelay =
+        Math.random() * 5 + "s";
 
-
-
-particle.style.left =
-Math.random()*100+"%";
-
-
-
-particle.style.animationDuration =
-(5 + Math.random()*10)+"s";
-
-
-
-particle.style.animationDelay =
-Math.random()*5+"s";
-
-
-
-particles.appendChild(particle);
-
-
+    fragment.appendChild(particle);
 }
+
+particles.appendChild(fragment);
