4d4252e7f710c79d758857866cc8206c89820bd5
topivanee <ivanegacherdi@gmail.com>
2026-07-25 11:08:31 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,65 @@
-const buttons = document.querySelectorAll("nav a");
+// Smooth scrolling
+
+document.querySelectorAll("nav a").forEach(link=>{
 
 
-buttons.forEach(btn=>{
+link.addEventListener("click",()=>{
 
-btn.addEventListener("click",()=>{
 
 document.querySelector(
-btn.getAttribute("href")
+link.getAttribute("href")
 )
 .scrollIntoView({
+
 behavior:"smooth"
-});
 
 });
 
+
 });
+
+
+});
+
+
+
+
+// Create particles
+
+
+const particles =
+document.getElementById("particles");
+
+
+
+for(let i = 0; i < 80; i++){
+
+
+let particle =
+document.createElement("div");
+
+
+
+particle.className="particle";
+
+
+
+particle.style.left =
+Math.random()*100+"%";
+
+
+
+particle.style.animationDuration =
+(5 + Math.random()*10)+"s";
+
+
+
+particle.style.animationDelay =
+Math.random()*5+"s";
+
+
+
+particles.appendChild(particle);
+
+
+}
